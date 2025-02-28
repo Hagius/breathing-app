8419210acd45b47ae735f4cea1c8d95c76f0e9af
Hagius <nicolas@hagius.de>
2025-02-28 12:26:59 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,88 @@
+const CACHE_NAME = 'hagius-breathing-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/logo.png',
+  '/monogram.png',
+  '/sounds/chime-inhale.mp3',
+  '/sounds/chime-hold.mp3',
+  '/sounds/chime-exhale.mp3',
+  '/icons/icon-72x72.png',
+  '/icons/icon-96x96.png',
+  '/icons/icon-128x128.png',
+  '/icons/icon-144x144.png',
+  '/icons/icon-152x152.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-384x384.png',
+  '/icons/icon-512x512.png'
+];
+
+// Install event - cache all static assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(ASSETS);
+      })
+      .catch(err => console.error('Cache open failed:', err))
+  );
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            console.log('Removing old cache', key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+// Fetch event - serve from cache, fall back to network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return the response from cache
+        if (response) {
+          return response;
+        }
+
+        // Clone the request because it's a one-time use stream
+        const fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest)
+          .then((response) => {
+            // Check if we received a valid response
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clone the response because it's a one-time use stream
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                // Add fetched resource to cache
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          })
+          .catch(() => {
+            // If both cache and network fail, we can provide a fallback
+            if (event.request.url.indexOf('.html') > -1) {
+              return caches.match('/index.html');
+            }
+          });
+      })
+  );
+});
